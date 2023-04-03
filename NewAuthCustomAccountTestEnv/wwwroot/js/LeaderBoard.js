70e7d563b15ffa75d905b7da886175f60fe80793
basgast2002 <94895624+basgast2002@users.noreply.github.com>
2023-04-03 10:17:14 +0200

    sync added + let to const leaderboard javascript

--- NewAuthCustomAccountTestEnv/wwwroot/js/LeaderBoard.js
+++ NewAuthCustomAccountTestEnv/wwwroot/js/LeaderBoard.js
@@ -1,18 +1,25 @@
 ﻿
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/LeaderBoardNotifyHub").build();
+const connection = new signalR.HubConnectionBuilder().withUrl("/LeaderBoardNotifyHub").build();
 
 connection.on("UpdateLeaderBoard", function () {
-    //AJAX post call naar update leaderboard
-    console.log("iets ontvangen tenminste");
+    
+    //console.log("iets ontvangen tenminste");
     window.location.reload();
+    
+    
 });
 
 connection.start()
     .then(function () {
+        //wanneer connectie gelukt is: log op console
         console.log("Connection started");
+        const date = new Date();
+        document.getElementById("latest-update-label").innerHTML = "Latest Sync: " + date.getHours() + ':' + date.getMinutes() + '/' + date.toLocaleDateString();
     })
     .catch(function (err) {
+        //wanneer connectie faalt: log de error op console
         console.error(err.toString());
+
     });
 
